992dff1ce441308a298ade8ac162806c05225cce
2543823560 <2543823560@qq.com>
2021-04-25 09:50:20 +0800

    first

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <a id="gg">AAAAA</a>
+    <div class="ggg">1323</div>
+    <Tab></Tab>
+  </div>
+</template>
+<script>
+import Tab from '../components/Tab'
+export default {
+  components:{
+    Tab
+  }
+}
+</script>
+<style lang="less" scoped>
+ /deep/ .about{
+   color: red;
+ 
+}
+</style>
